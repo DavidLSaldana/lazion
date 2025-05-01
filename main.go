3dc1747c30d7000f590fa0fc5b989615c0601b14
DavidLSaldana <dleosaldana@gmail.com>
2025-05-01 08:43:37 -0700

    a couple of small additions before work

--- main.go
+++ main.go
@@ -2,8 +2,21 @@ package main
 
 import (
 	"fmt"
+	"os"
+
+	"github.com/charmbracelet/bubbles/textinput"
+	tea "github.com/charmbracelet/bubbletea"
+)
+
+const (
+// menu fields go here, will be iota'd
 )
 
 func main() {
 	fmt.Println("Hello World!")
+}
+
+type model struct {
+	inputs  []textinput.Model
+	focused int
 }
